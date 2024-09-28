789452a3e5b4ca7b4b7612aee8f214b29f644468
bluegalaxy614 <starandrii170@gmail.com>
2024-09-28 18:15:19 +0900

    update : main & sub categories button

--- app/page.tsx
+++ app/page.tsx
@@ -46,6 +46,8 @@ export default function Home() {
     () => Array.from(selectedKeys).join(', ').replaceAll('_', ' '),
     [selectedKeys],
   );
+
+  
   return (
     <>
       <ImageSlider slides={slides} />
---
+++ app/store.ts
@@ -0,0 +1,6 @@
+import { create } from 'zustand';
+
+export const useStore = create((set) => ({
+    selectedCategories: [],
+    setSelectedCategories: (categories: string[]) => set({ selectedCategories: categories })
+}))
