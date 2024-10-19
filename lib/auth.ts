2f435e98d67223a9348239a9654345bca4a984ef
bluegalaxy614 <starandrii170@gmail.com>
2024-10-19 17:15:20 +0900

    url

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { RegisterData, LoginData, ForgotPasswordData } from '@/types/libType';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL || 'http://199.188.238.224:5000';
 
 const authService = {
   register: async (data: RegisterData) => {
